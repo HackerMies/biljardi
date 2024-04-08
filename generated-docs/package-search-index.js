25b6e093bad39db12a38987e5236cfffacace5ca
HackerMies <o-k@hotmail.fi>
2024-04-08 13:28:08 +0300

    Initial commit

---
+++ generated-docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.example.biljardi","l":"org.example.biljardi"}];updateSearchResults();
---
+++ generated-docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.biljardi","l":"biljardivarausolio"},{"p":"org.example.biljardi","l":"VarausKayttoliittyma"}];updateSearchResults();
